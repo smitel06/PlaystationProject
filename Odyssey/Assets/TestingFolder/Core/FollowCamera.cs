c51e5d615de48d9defc517daca4d08db8136b014
Eli <smitel06@hotmail.com>
2021-09-18 09:51:29 +0930

    New project and Movement added

---
+++ Odyssey/Assets/TestingFolder/Core/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    //A camera setup to follow the player
+
+    public Transform target;
+
+    void Update()
+    {
+        //set position of THIS object to targets position
+        transform.position = target.transform.position;
+    }
+}
---
+++ Odyssey/Assets/TestingFolder/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //use this script to control the player through input
+
+    //Movement variables and components
+    Rigidbody rb;
+    //-------------------
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        movePlayer();
+    }
+
+    //move the player according to controller input
+    void movePlayer()
+    {
+        // use this for animation velocity and blend tree
+        float maxVelocity = 5; 
+        
+        //get inputs and magnitude
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        float inputMagnitude = input.magnitude;
+        //get velocity
+        float velocity = inputMagnitude * maxVelocity;
+
+        //set velocity of the rigidbody this is what moves the character
+        rb.velocity = (input * velocity);
+
+    }
+}
