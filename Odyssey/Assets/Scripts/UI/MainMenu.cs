3b75925efd560eb90e98c544999dcee455a873ff
Eli <smitel06@hotmail.com>
2021-10-01 18:22:50 +0930

    input mainmenu

--- Odyssey/Assets/Scripts/UI/MainMenu.cs
+++ Odyssey/Assets/Scripts/UI/MainMenu.cs
@@ -2,9 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    //buttons we want to start on when opening each menu
+    public GameObject startMenuFirstButton, optionsFirstButton, optionsClosedButton;
+
+    private void Start()
+    {
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new selected object which will be the start button
+        EventSystem.current.SetSelectedGameObject(startMenuFirstButton);
+    }
+
+    private void Update()
+    {
+        if((Input.GetButtonDown("Submit")))
+        {
+            if(EventSystem.current.currentSelectedGameObject == startMenuFirstButton)
+            {
+                PlayGame();
+            }
+            else if (EventSystem.current.currentSelectedGameObject == startMenuFirstButton)
+            {
+
+            }
+        }
+    }
+
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -16,24 +44,7 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    private void Update()
-    {
-        checkForInput();
-    }
+    
 
-    //Input with controller goes underneath here
-    void checkForInput()
-    {
-        
-        if (Input.GetButtonDown("Submit"))
-        {
-            //this is A or X button
-        }
-        else if (Input.GetButtonDown("Cancel"))
-        {
-            //this is A or X button
-        }
-
-    }
 
 }
