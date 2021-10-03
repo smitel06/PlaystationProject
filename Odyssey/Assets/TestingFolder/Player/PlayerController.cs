cdfd1caddad42a46004e2ec134354d5fa5ea1d9e
Eli <smitel06@hotmail.com>
2021-10-03 23:19:10 +1030

    dash

--- Odyssey/Assets/TestingFolder/Player/PlayerController.cs
+++ Odyssey/Assets/TestingFolder/Player/PlayerController.cs
@@ -6,70 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     //use this script to control the player through input
 
-    //Movement variables and components
+    //components
     Rigidbody rb;
-    //-------------------
-    //combat variables
-    bool attacking;
-    public GameObject currentWeapon;
-    float attackTime;
-    float waitTime;
-
-    //animator things
     Animator animator;
+    //common Variables
+    Vector3 input;
+    //animator things
+
     float idleTimer;
     float idleSwitchWait;
     //randomly set idle
     [SerializeField] int randomIdle;
 
+    
+
+   
+
     private void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        attackTime = 0.5f;
 
         idleSwitchWait = 10.0f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
+        //get inputs and magnitude
+        input = new Vector3(Input.GetAxis("HorizontalMovement"), 0, Input.GetAxis("VerticalMovement"));
+        dash();
         movePlayer();
-
-        
-        //
-        //Attack();
-
     }
 
     private void Attack()
     {
-        //if animation is not playing
-        if (!GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-        {
-            attacking = false;
-            //if x is pressed
-            if (Input.GetButtonDown("Attack"))
-            {
-                //set time till we want hit to occur
-                waitTime = attackTime + Time.time;
-
-                rb.velocity = new Vector3(0, 0, 0);
-                GetComponent<Animator>().SetTrigger("Attack");
-            }
-            else
-                movePlayer();
-        }
-        else
-            attacking = true;
-
-        //stop hits from happening unless we want them to 
-        if (attacking == true && waitTime < Time.time)
-        {
-            currentWeapon.GetComponent<weaponScript>().attackOn = true;
-
-        }
-        //else
-        //    currentWeapon.GetComponent<weaponScript>().attackOn = false;
+        
     }
 
     //move the player according to controller input
@@ -80,9 +51,6 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Moving", true);
             // use this for animation velocity and blend tree
             float maxVelocity = 10;
-
-            //get inputs and magnitude
-            Vector3 input = new Vector3(Input.GetAxis("HorizontalMovement"), 0, Input.GetAxis("VerticalMovement"));
             float inputMagnitude = input.magnitude;
             //get velocity
             float velocity = inputMagnitude * maxVelocity;
@@ -94,11 +62,8 @@ public class PlayerController : MonoBehaviour
             if (inputMagnitude > 0)
                 rb.transform.rotation = Quaternion.LookRotation(input, rb.transform.up);
 
-            //update animator
+           animator.SetFloat("Blend", inputMagnitude);
             
-                animator.SetFloat("Blend", inputMagnitude);
-            //else
-            //    animator.SetFloat("Blend", 0);
         }
         else
         {
@@ -118,6 +83,18 @@ public class PlayerController : MonoBehaviour
             animator.SetInteger("RandomIdleNum", randomIdle);
             idleTimer = 0;
         }
-        
+    }
+
+    void dash()
+    {
+        if (Input.GetButtonDown("Dash"))
+        {
+            float dashDistance = 10f;
+            if(!Physics.Raycast(transform.position, input, dashDistance))
+            {
+                Vector3 beforeDashPosition = transform.position;
+                transform.position += input * dashDistance;
+            }
+        }   
     }
 }
