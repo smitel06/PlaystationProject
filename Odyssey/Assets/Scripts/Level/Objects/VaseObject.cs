bc416a2a669c799e053a8aea1ed469e262b2c5a5
Eli <smitel06@hotmail.com>
2021-11-02 16:27:42 +1030

    vase script complete

--- Odyssey/Assets/Scripts/Level/Objects/VaseObject.cs
+++ Odyssey/Assets/Scripts/Level/Objects/VaseObject.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class VaseObject : MonoBehaviour
 {
     [SerializeField] Animator anim;
-    
-    private void OnTriggerEnter(Collider collision)
+    int randomDrop;
+
+    private void OnEnable()
     {
-        if(collision.gameObject.tag == "PlayerWeapon")
+        randomDrop = Random.Range(1, 5);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PlayerWeapon")
         {
             anim.enabled = true;
+            dropCoin();
         }
+    }
+
+    private void dropCoin()
+    {
+        
+
+        if (randomDrop < 2)
+        {
+            GameObject coin = Instantiate(GameAssets.i.coinDrop, new Vector3(transform.position.x, 1, transform.position.z), Quaternion.identity) as GameObject;
+            coin.GetComponent<CoinDrop>().SetRandomRange(20, 35);
+        }
+        foreach (Collider c in GetComponents<Collider>())
+        {
+            c.enabled = false;
+        }
+
     }
 }
