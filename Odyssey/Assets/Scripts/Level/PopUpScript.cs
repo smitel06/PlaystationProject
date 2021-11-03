640eb2d09137e60bcb577f6d6efe95bd69d8a50e
Eli <smitel06@hotmail.com>
2021-11-03 16:22:17 +1030

    popup script plus merchant room

---
+++ Odyssey/Assets/Scripts/Level/PopUpScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpScript : MonoBehaviour
+{
+    float distance;
+    [SerializeField] float minDistance;
+    [SerializeField]GameObject player;
+    [SerializeField] GameObject popUp;
+    
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance < minDistance)
+        {
+            popUp.SetActive(true);
+
+        }
+        else
+        {
+            popUp.SetActive(false);
+        }
+    }
+}
---
+++ Odyssey/Assets/Scripts/UI/HUD/PurchaseItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseItem : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    int coins;
+    float distance;
+    [SerializeField] float minDistance;
+    [SerializeField] int cost;
+    [SerializeField] GameObject popUp;
+    Buffs buffs;
+    bool canBuy;
+    
+    void Start()
+    {
+        coins = player.GetComponent<PlayerCurrencies>().coins;
+        buffs = player.GetComponent<Buffs>();
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance < minDistance)
+        {
+            popUp.SetActive(true);
+            canBuy = true;
+        }
+        else
+        {
+            popUp.SetActive(false);
+            canBuy = false;
+        }
+
+        if(canBuy && Input.GetAxis("Interact") > 0)
+        {
+            if(cost == 250)
+            {
+                if(coins >= 250)
+                {
+                    if(!buffs.lifeBonus)
+                    {
+                        buffs.lifeBonus = true;
+                        player.GetComponent<PlayerCurrencies>().setCoins(-250);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
