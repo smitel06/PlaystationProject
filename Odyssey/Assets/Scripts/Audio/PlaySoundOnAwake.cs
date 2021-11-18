8c26a1fdaf468b03bd9d449fd342dd6976b9628d
Eli <smitel06@hotmail.com>
2021-11-18 23:01:56 +1030

    sound script

--- Odyssey/Assets/Scripts/Audio/PlaySoundOnAwake.cs
+++ Odyssey/Assets/Scripts/Audio/PlaySoundOnAwake.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlaySoundOnAwake : MonoBehaviour
 {
     GameObject player;
+    [SerializeField] AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
